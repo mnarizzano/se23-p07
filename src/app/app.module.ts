53cb087d8f45171ea58d09cbe5f613e8d071c5bd
Martina <martinamaione2000@gmail.com>
2023-08-14 15:11:18 +0200

    Primo commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PersonalDataComponent } from './personal-data/personal-data.component';
+import { SavedPSComponent } from './saved-ps/saved-ps.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { MapComponent } from './map/map.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  // Aggiungi altre route per le tue pagine
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    PersonalDataComponent,
+    SavedPSComponent,
+    StatisticsComponent,
+    MapComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/saved-ps/saved-ps.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SavedPSComponent } from './saved-ps.component';
+
+describe('SavedPSComponent', () => {
+  let component: SavedPSComponent;
+  let fixture: ComponentFixture<SavedPSComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SavedPSComponent]
+    });
+    fixture = TestBed.createComponent(SavedPSComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
