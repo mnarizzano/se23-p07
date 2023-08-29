664ae147dae2eca8adfd63ff0eca51cf057f2e8e
martinamaione <martinamaione2000@gmail.com>
2023-08-29 17:50:13 +0200

    Merged project (task 1 and 2.1-2.2)

--- src/main/src/app/app-routing.module.ts
+++ src/main/src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './verify-email/verify-email.component';
 import { HomeComponent } from './home/home.component';
 import { PersonalDataComponent } from './personal-data/personal-data.component';
 import { SavedPSComponent } from './saved-ps/saved-ps.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { MapComponent } from './mappa/map.component';
+import { AuthGuard } from './shared/guard/auth.guard';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  {
-    path: 'home', component: HomeComponent,
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'register-user', component: SignUpComponent },
+  { 
+    path: 'home', component: HomeComponent, canActivate: [AuthGuard] ,
     children: [
       { path: 'personalData', component: PersonalDataComponent },
       { path: 'saved-ps', component: SavedPSComponent },
@@ -18,15 +24,15 @@ const routes: Routes = [
       { path: 'map', component: MapComponent }
     ]
   },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'verify-email-address', component: VerifyEmailComponent },
   { path: 'map', component: MapComponent },
   { path: 'personalData', component: PersonalDataComponent },
   { path: 'saved-ps', component: SavedPSComponent },
   { path: 'statistics', component: StatisticsComponent }
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/main/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyAAMSYnoPsaqzL5ONQZdZpyMRcirWV2yUY",
+      authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+      projectId: "prova-f6a0f",
+      storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+      messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+      appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    }
+  };
