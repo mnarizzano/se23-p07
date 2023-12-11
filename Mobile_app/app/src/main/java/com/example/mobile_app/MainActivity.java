221254dedeee911f331d8a193c8873ca35ee24bc
MartinaMaione <86767465+MartinaMaione@users.noreply.github.com>
2023-12-11 12:04:18 +0100

    Revert "Delete Mobile_app directory"
    This reverts commit 99d5cde622d3d3fe8f568c0cf997ae2f2385e61c.

---
+++ Mobile_app/app/src/main/java/com/example/mobile_app/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.mobile_app;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+
+
+
+
+
+
+
+}
+
---
+++ Mobile_app/app/src/main/java/com/example/mobile_app/SignupActivity.java
@@ -0,0 +1,7 @@
+package com.example.mobile_app;
+
+import android.app.Activity;
+
+public class SignupActivity extends Activity {
+
+}
