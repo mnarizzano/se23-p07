e15019b6516570f39edc73bc23a11279f8c69fc4
MartinaMaione <86767465+MartinaMaione@users.noreply.github.com>
2024-01-18 17:30:30 +0100

    Update save-confirmation.component.ts

--- src/Web_app/src/app/save-confirmation/save-confirmation.component.ts
+++ src/Web_app/src/app/save-confirmation/save-confirmation.component.ts
@@ -6,16 +6,20 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
   templateUrl: './save-confirmation.component.html',
   styleUrls: ['./save-confirmation.component.css']
 })
+
+  // Class for managing the saving of parking slots 
 export class SaveConfirmationComponent {
   @Output() onConfirm: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   constructor(public bsModalRef: BsModalRef) {}
 
+  // Manages the click on the confirm button
   confirmSave() {
     this.onConfirm.emit(true); 
     this.bsModalRef.hide(); 
   }
 
+  // Manages the click on the cancel button
   cancelSave() {
     this.onConfirm.emit(false); 
     this.bsModalRef.hide(); 
